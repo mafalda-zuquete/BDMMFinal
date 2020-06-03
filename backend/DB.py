57f19b6c53738a4473be4e9671d6fd7dbf67760c
mafalda-zuquete <55488606+mafalda-zuquete@users.noreply.github.com>
2020-06-03 17:11:21 +0100

    credentials

--- backend/DB.py
+++ backend/DB.py
@@ -1,9 +1,9 @@
 from pymongo import MongoClient
 
 host="rhea.isegi.unl.pt"
-port="27017"
-user="mongo_classes_rhea"
-password="bW9uZ29fY2xhc3Nlc19yaGVh"
+port="28011"
+user="GROUP_11"
+password="MTM0MzQ5MzI3NjY4ODM1MzQ2ODc2OTMzMjgyNjQ3NjYyNDcyNjUw"
 protocol="mongodb"
 
 client = MongoClient(f"{protocol}://{user}:{password}@{host}:{port}")
